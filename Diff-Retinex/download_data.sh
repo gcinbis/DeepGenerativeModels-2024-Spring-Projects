9b5cbf3a7d2f6bbf56cc62227cdc4ff7d89d225a
HamzaEtcibasi <80333074+HamzaEtcibasi@users.noreply.github.com>
2024-05-31 00:24:26 +0300

    Update download_data.sh
    download pretrained models here.

--- Diff-Retinex/download_data.sh
+++ Diff-Retinex/download_data.sh
@@ -9,6 +9,12 @@ output_file="lol_dataset.zip"
 # İndirme işlemi
 wget "$file_url" -O "$output_file"
 
+wget --no-check-certificate "https://drive.google.com/file/d/1nOLIiVGoioCYgXrwlPduWcqGglzo3E0d/view?usp=sharing"
+
+wget --no-check-certificate "https://drive.google.com/file/d/1Mftlzs3MHkFOUTQjXYDe2iF35eJnj9gQ/view?usp=sharing"
+
+wget --no-check-certificate "https://drive.google.com/file/d/1EMwq_eHhZy2xmjUHhRCDnhfQDSvWcz99/view?usp=sharing"
+
 # İndirilen dosyayı çıkartma
 unzip "$output_file" -d ./
 
